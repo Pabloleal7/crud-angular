d5f9bec168d622fa205de1924936d28675575fee
Pablo Leal <pabloleal7@hotmail.com>
2021-09-02 10:57:05 -0300

    forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  mensagem = 'eita porra';
+  forms: FormGroup = new FormGroup({
+    description : new FormControl('')
+  })
+
+  submit(){
+    console.log(this.forms.value)
+  }
 }
